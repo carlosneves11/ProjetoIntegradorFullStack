7b3a6989984d8810153c2ad49239801b2790019f
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-11-13 15:14:24 -0300

    create express server

---
+++ app.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running at http://localhost:${process.env.PORT}`);
+})
