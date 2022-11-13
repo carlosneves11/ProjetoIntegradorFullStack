76861145722bba955829a2d407c428a53a1f1f46
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-11-13 18:59:01 -0300

    Create view and WIP page for /login

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 require('dotenv').config()
 const express = require('express');
 const consign = require('consign');
+const path = require('path');
 const app = express();
+
+// Views
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 
 // Consign
@@ -10,6 +15,8 @@ consign({})
 .then('controllers')
 .then('routes')
 .into(app)
+
+
 
 // Start up server
 app.listen(process.env.PORT, () => {
