38d0c0c18d5b7e0a7d7b2ba29775b8f2fed1a454
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-11-15 09:29:16 -0300

    create route controller and remove view of register

--- controllers/register.js
+++ controllers/register.js
@@ -1,8 +1,15 @@
 module.exports = (app) => {
     const RegisterController = {
         view: (req, res, next) => {
-            res.render('register')
-        }        
+            // [para fazer] adicionar view
+            //res.render('register')
+            res.end()
+        },
+        create: (req, res, next) => {
+            const { name, email, password, type } = req.body
+            // [para fazer] criar registro no mongodb
+            res.end()
+        }
     }
 
     return RegisterController
--- routes/register.js
+++ routes/register.js
@@ -1,4 +1,5 @@
 module.exports = (app) => {
     const { register } = app.controllers
     app.get('/register', register.view)
+    app.post('/register', register.create)
 }
