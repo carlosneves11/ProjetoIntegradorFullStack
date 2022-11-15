ee99f629a5f13a7b6714ce07fcd8972eee14736e
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-11-15 06:17:54 -0300

    create register route and temp view

---
+++ controllers/register.js
@@ -0,0 +1,10 @@
+module.exports = (app) => {
+    const RegisterController = {
+        view: (req, res, next) => {
+
+        },
+        
+    }
+
+    return RegisterController
+}
---
+++ routes/register.js
@@ -0,0 +1,4 @@
+module.exports = (app) => {
+    const { register } = app.controllers
+    app.get('/register')
+}
