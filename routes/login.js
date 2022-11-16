5bec5485cd941ce9dec37761b7b782b3110be814
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-11-16 00:21:41 -0300

    delete everything

--- routes/login.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app) => {
-    const { login } = app.controllers
-
-    app.get('/', (req, res, next) => {
-        res.redirect('/login')
-    })
-
-    app.get('/login', login.login)
-}
