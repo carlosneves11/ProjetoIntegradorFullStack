785c242ee11a87125a990de1cdf38286e8c1a4b2
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-11-16 00:10:07 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express');
+const consign = require('consign');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const path = require('path');
+const app = express();
+
+
+// Views
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, '/public')))
+
+// Session
+app.use(cookieParser(process.env.ACCESS_TOKEN_SESSION))
+app.use(session())
+
+// Body Parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Consign
+consign({})
+.include('')
+.then('controllers')
+.then('routes')
+.into(app)
+
+
+// Start up server.
+app.listen(process.env.PORT, () => {
+    console.log(`Server running at http://localhost:${process.env.PORT}`);
+})
+console.log('CCSM Vagas')
