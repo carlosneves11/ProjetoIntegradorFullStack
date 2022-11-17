a82c5e20a0a8b5900632dd6bb0bede78627bddfd
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-11-16 23:17:25 -0300

    create register of user to database

--- controllers/register.js
+++ controllers/register.js
@@ -1,13 +1,60 @@
 module.exports = (app) => {
     const RegisterController = {
         view: (req, res, next) => {
-            // [para fazer] adicionar view
-            //res.render('register')
-            res.end()
+            // usando view de teste
+            const registerModel = app.models.register
+
+            registerModel.find({})
+            .then((users) => { 
+                console.log(users)
+            })
+            
+            res.render('teste_register')
         },
         create: (req, res, next) => {
             const { name, email, password, type } = req.body
             // [para fazer] criar registro no mongodb
+
+            const registerModel = app.models.register
+
+            registerModel.find({})
+            .then((users) => { 
+                console.log(users)
+            })
+
+            user = {
+                profile: {
+                    avatar_url: null,
+                    name: name,
+                    birth: null,
+                    email: null,
+                    pronoun: null,
+                    title: null,
+                    current_position: null,
+                    sector: [],
+                    phone: [],
+                    location: []
+                }, 
+                posts: {},
+                jobs: {},
+                configurations: {
+                    email: email,
+                    password: password,
+                    type: type,
+                    theme: "light"
+                },
+                email: email, 
+                notifications: {}
+            }
+
+            registerModel.create(user)
+            .then(()=>{
+                console.log(user)
+            })
+            .catch(err => console.log(err))
+
+            
+
             res.end()
         }
     }
