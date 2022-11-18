7950a5327f388fbcdecbdf4064bbd870dc009adc
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-11-18 00:07:39 -0300

    creates register and encrypt password
    Creates a useful register to database and encrypt the password

--- controllers/register.js
+++ controllers/register.js
@@ -1,64 +1,66 @@
+const bcrypt = require('bcrypt')
+
 module.exports = (app) => {
     const RegisterController = {
         view: (req, res, next) => {
             // usando view de teste
-            const registerModel = app.models.register
-
-            registerModel.find({})
-            .then((users) => { 
-                console.log(users)
-            })
-            
             res.render('teste_register')
         },
         create: (req, res, next) => {
             const { name, email, password, type } = req.body
-            // [para fazer] criar registro no mongodb
-
             const registerModel = app.models.register
 
-            registerModel.find({})
+            registerModel.findOne({email: email.toLowerCase()})
             .then((users) => { 
-                console.log(users)
-            })
+                if (users == null) {
+                    user = {
+                        profile: {
+                            avatar_url: null,
+                            name: name,
+                            birth: null,
+                            email: null,
+                            pronoun: null,
+                            title: null,
+                            current_position: null,
+                            sector: [],
+                            phone: [],
+                            location: []
+                        }, 
+                        posts: {},
+                        jobs: {},
+                        configurations: {
+                            email: email.toLowerCase(),
+                            password: password,
+                            type: type,
+                            theme: "light"
+                        },
+                        email: emailtoLowerCase(), 
+                        notifications: {}
+                    }
 
-            user = {
-                profile: {
-                    avatar_url: null,
-                    name: name,
-                    birth: null,
-                    email: null,
-                    pronoun: null,
-                    title: null,
-                    current_position: null,
-                    sector: [],
-                    phone: [],
-                    location: []
-                }, 
-                posts: {},
-                jobs: {},
-                configurations: {
-                    email: email,
-                    password: password,
-                    type: type,
-                    theme: "light"
-                },
-                email: email, 
-                notifications: {}
-            }
+                    // encriptografa a senha
+                    bcrypt.hash(password, 10, (errBcrypt, hash) => {
+                        if (errBcrypt) return res.status(500).send({error: errBcrypt})
 
-            registerModel.create(user)
-            .then(()=>{
-                console.log(user)
+                        user.configurations.password = hash
+
+                        // Cria o usuário
+                        registerModel.create(user)
+                        .then(()=>{
+                            console.log(`[MongoDB] User account "${name}" created`)
+                        })
+                        .catch(err => console.log(err))
+
+                        return res.status(201).redirect('/login')
+                    })
+                }
+                else {
+                    console.log(`[MongoDB] User account "${name}" already exists`)
+                    return res.status(401).redirect('/register')
+                }
             })
             .catch(err => console.log(err))
-
-            
-
-            res.end()
         }
     }
-    
-
     return RegisterController
 }
